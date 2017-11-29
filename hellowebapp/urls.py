926f49d683de55bf34588d5e569523bad906bc6f
Laura Salinas <laurabug2010@gmail.com>
2017-11-29 11:28:52 -0500

    adding new templates

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', 'collection.views.index', name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
