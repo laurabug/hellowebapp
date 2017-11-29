a4bf14ad586b375e9ebd193b9ee2b056f3b13f6f
Laura Salinas <laurabug2010@gmail.com>
2017-11-29 11:33:04 -0500

    adding nav

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', 'collection.views.index', name='home'),
