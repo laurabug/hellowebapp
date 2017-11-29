f42459ba7c9b34ad4f3190a935cb9d8982bcb0e4
Laura Salinas <laurabug2010@gmail.com>
2017-11-29 15:37:43 -0500

    adding a form

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms	import ThingForm
 from collection.models import Thing
 # Create your views here.
 
@@ -16,4 +17,26 @@ def thing_detail(request, slug):
 	# and pass to the template
 	return render(request, 'things/thing_detail.html', {
 		'thing': thing,
+		})
+
+def edit_thing(request, slug):
+	# grab the object 
+	thing = Thing.objects.get(slug=slug)
+	# set the form we are using
+	form_class = ThingForm
+	# if we are coming to this view from a submitted form,
+	if request.method == 'POST':
+		# grab the data from the submitted from
+		form = form_class(data=request.POST, instance=thing)
+		if form.is_valid():
+			#save the new data
+			form.save()
+			return redirect('thing_detail', slug=thing.slug)
+	# otherwise just create the form
+	else:
+		form = form_class(instance=thing)
+	# and render the template
+	return render(request, 'things/edit_thing.html', {
+		'thing': thing,
+		'form': form,
 		})
